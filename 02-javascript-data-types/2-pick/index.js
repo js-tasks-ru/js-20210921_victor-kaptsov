5f032924c29aa938e3b325dbabf34a519911fa6c
theConscience <vinton.mail@gmail.com>
2021-09-27 20:52:39 +0300

    Add 2nd task of 2nd module solution
    - implement pick() function for cloning of object with selected
      fields only

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,8 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  return fields.reduce((acc, f) => {
+    if (f in obj) acc[f] = obj[f];
+    return acc;
+  }, {});
 };
