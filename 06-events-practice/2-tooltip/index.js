7737ffb5c8ceb03df902702722e10713e565b706
theConscience <vinton.mail@gmail.com>
2021-10-14 17:10:46 +0300

    Add initial js for 2nd task of 6th module

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,55 @@
 class Tooltip {
+  element;
+  static tooltip;
 
+  constructor() {
+    if (Tooltip.tooltip) return Tooltip.tooltip;
+    this.onPointerOverTooltip = this.onPointerOverTooltip.bind(this);
+    this.render();
+  }
+
+  render(template = this.template) {
+    this.element = htmlToElement(template);
+  }
+
+  get template() {
+    return `<div class="tooltip">This is tooltip</div>`;
+  }
+
+  initialize() {
+    this.addListeners();
+  }
+
+  addListeners() {
+    document.addEventListener("pointerover", this.onPointerOverTooltip);
+  }
+
+  removeListeners() {
+    document.removeEventListener("pointerover", this.onPointerOverTooltip);
+  }
+
+  onPointerOverTooltip(evt) {
+    console.log("Evt:", evt);
+  }
+
+  remove() {
+    if (this.element) this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.removeListeners();
+    this.element = null;
+  }
 }
 
 export default Tooltip;
+
+function htmlToElement(html) {
+  // const template = document.createElement("template");
+  // template.innerHTML = html.trim();
+  // return template.content.firstChild;
+  const div = document.createElement("div");
+  div.insertAdjacentHTML("afterbegin", html.trim());
+  return div.firstChild;
+}
--- 06-events-practice/2-tooltip/index.spec.js
+++ 06-events-practice/2-tooltip/index.spec.js
@@ -1,10 +1,10 @@
-import tooltip from './index.js';
+import Tooltip from "./index.js";
 
-describe('events-practice/tooltip', () => {
+describe("events-practice/tooltip", () => {
   const tooltip = new Tooltip();
 
   beforeEach(() => {
-    const container = document.createElement('div');
+    const container = document.createElement("div");
 
     container.innerHTML = `<div id="container" data-tooltip="bar-bar-bar">
       Aperiam consectetur dignissimos dolores ex mollitia.
@@ -18,47 +18,47 @@ describe('events-practice/tooltip', () => {
     tooltip.destroy();
   });
 
-  it('should be rendered correctly', () => {
-    tooltip.render('');
+  it("should be rendered correctly", () => {
+    tooltip.render("");
 
     expect(tooltip.element).toBeVisible();
     expect(tooltip.element).toBeInTheDocument();
   });
 
-  it('should be shown when moving over the element with data-tooltip attribute', () => {
-    const pointerover = new MouseEvent('pointerover', {
-      bubbles: true
+  it("should be shown when moving over the element with data-tooltip attribute", () => {
+    const pointerover = new MouseEvent("pointerover", {
+      bubbles: true,
     });
 
-    const container = document.getElementById('container');
+    const container = document.getElementById("container");
 
     container.dispatchEvent(pointerover);
 
     expect(tooltip.element).toBeVisible();
     expect(tooltip.element).toBeInTheDocument();
-    expect(tooltip.element).toHaveTextContent('bar-bar-bar');
+    expect(tooltip.element).toHaveTextContent("bar-bar-bar");
   });
 
-  it('should be hidden when moving out from the element with data-tooltip attribute', () => {
-    const pointermove = new MouseEvent('pointerout', {
-      bubbles: true
+  it("should be hidden when moving out from the element with data-tooltip attribute", () => {
+    const pointermove = new MouseEvent("pointerout", {
+      bubbles: true,
     });
 
-    const container = document.getElementById('container');
+    const container = document.getElementById("container");
 
     container.dispatchEvent(pointermove);
 
     expect(tooltip.element).not.toBeInTheDocument();
   });
 
-  it('should be constructed via singleton pattern', () => {
+  it("should be constructed via singleton pattern", () => {
     const tooltip1 = new Tooltip();
     const tooltip2 = new Tooltip();
 
     expect(tooltip1 === tooltip2).toBeTruthy();
   });
 
-  it('should have ability to be destroyed', () => {
+  it("should have ability to be destroyed", () => {
     tooltip.destroy();
 
     expect(tooltip.element).not.toBeInTheDocument();
